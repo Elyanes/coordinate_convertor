6f3d74d56e483e3bfd4321a4994b481e0de4e3e9
Elyanes <rauchmateusz@gmail.com>
2022-06-15 01:36:49 +0200

    buttons

--- __main__.py
+++ __main__.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 import convertor
+import generate_window
 import os
-from os.path import join as osj
+
 
 def main():
-    input_file = "C:\\Projets\\coordinate_convertor\\examples\\testfilé.nc"
-    input_file_name = os.path.basename(input_file).split(".")[0]
-    output_folder = "C:\\Projets\\coordinate_convertor\\examples"
-    output_file = osj(output_folder, input_file_name + "_output.geo")
-    convertor.convertor(input_file, output_file)
+    generate_window.create_window()
 
 
 if __name__ == "__main__":
--- generate_window.py
+++ generate_window.py
@@ -1,20 +1,59 @@
 # -*- coding: utf-8 -*-
+from urllib import response
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
-
+from PySide6.QtWidgets import QApplication, QFileDialog, QPushButton, QLabel, QVBoxLayout, QWidget, QComboBox
+import __main__
 import sys
+import convertor
+import os
+from os.path import join as osj
 
-from random import randint
+class MainWindow(QWidget):
 
-class MainWindow(QMainWindow):
+   def __init__(self):
+      super().__init__()
+      self.window_width, self.window_height = 400, 100
+      self.setMinimumSize(self.window_width, self.window_height)
+      self.setWindowTitle("Coordinates Convertor")
 
-    def __init__(self):
-        super().__init__()
-        self.w = None
-        button = QPushButton("Push for Window")
-        self.setWindowTitle("Coordinates Convertor")
-        self.setFixedSize(QSize(600, 300))
-        self.setCentralWidget(button)
+      layout = QVBoxLayout()
+      self.setLayout(layout)
+      
+      file_button = QPushButton("Input File")
+      file_button.clicked.connect(self.get_input_file)
+      layout.addWidget(file_button)
+
+      folder_button = QPushButton("Output Folder")
+      folder_button.clicked.connect(self.get_output_folder)
+      layout.addWidget(folder_button)
+
+      # run_button = QPushButton("Run")
+      # run_button.clicked.connect(self.clicked_run_button)
+      # layout.addWidget(run_button)
+
+
+   def get_input_file(self):
+      response = QFileDialog.getOpenFileName(
+         parent = self,
+         caption = "Select a data file",
+         dir = os.getcwd()
+         )
+      return response[0]
+
+   def get_output_folder(self):
+      response = QFileDialog.getExistingDirectory(
+         self,
+         caption = "Select an output folder"
+         )
+      return response
+
+   # def clicked_run_button(self):
+   #    self.input_file_name = os.path.basename(self.input_file).split(".")[0]
+   #    self.output_file = osj(self.output_folder, self.input_file_name + "_output.geo")
+   #    convertor.convertor(self.input_file, self.output_file)
+
+   #    self.button_run.setGeometry(100,100,50,50)
+
 
 def create_window():
    app = QApplication(sys.argv)
